f795d897c84eb4b9d7f5a15076c7723edc546355
PrajyotDhat <108653611+PrajyotDhat@users.noreply.github.com>
2023-02-02 13:03:56 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+
+   const[firstName,setFirstName]=useState();
+   const[lastName,setLastName]=useState();
+   const[names,setNames]=useState([]);
+
+   useEffect(()=>{
+    const data=localStorage.getItem("names");
+    if(data){
+      setNames(JSON.parse(data));
+    }
+   },[]);
+   
+   const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(!firstName || !lastName)
+    return;
+
+    setNames([...names,{firstName,lastName}]);
+    setFirstName("");
+    setLastName("");
+   };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main">
+      <h2>Full Name Details</h2>
+       <form onSubmit={handleSubmit}>
+       <div>
+        <label>First Name:
+        <input type="text" placeholder="Enter Your First Name" required value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+        </label>
+        </div>
+        <div>
+        <label>Last Name:
+        <input type="text" placeholder="Enter Your Last Name" required value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+        </label>
+        </div>
+        <div className="btn">
+          <button type='submit'>Submit</button>
+        </div>
+       </form>
+      </div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            names.map((name,index)=>(
+              <tr key={index}>
+                <td>{name.firstName}</td>
+                <td>{name.lastName}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
